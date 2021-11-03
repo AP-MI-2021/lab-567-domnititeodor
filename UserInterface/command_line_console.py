fc37c4f457bd1ccaf9637c53dbffcb643e470b2c
domnititeodor <domnitit@gmail.com>
2021-11-03 13:26:37 +0200

    Meniu nou

---
+++ UserInterface/command_line_console.py
@@ -0,0 +1,76 @@
+from Domain.vanzare import creeaza_vanzare
+from Logic.crud import create, delete, update
+from UserInterface.console import handle_show_all
+
+
+def read_list():
+    lst = []
+    lst_str = input("Introduceti comanda respectand instructiunile date: ")
+    lst_str_split = lst_str.split('; ')
+    for comanda in lst_str_split:
+        lst.append(comanda)
+    return lst
+
+
+def handle_newadd(vanzari, comanda):
+    try:
+        id = int(comanda[1])
+        titlu = comanda[2]
+        gen = comanda[3]
+        pret = float(comanda[4])
+        tip = comanda[5]
+        return create(vanzari, id, titlu, gen, pret, tip)
+    except ValueError as ve:
+        print('Eroare: ', ve)
+
+    return vanzari
+
+
+def handle_newdelete(vanzari, comanda):
+    try:
+        id = int(comanda[1])
+        vanzari = delete(vanzari, id)
+        return vanzari
+    except ValueError as ve:
+        print('Eroare: ', ve)
+    return vanzari
+
+
+def handle_newupdate(vanzari, comanda):
+    try:
+        id = int(comanda[1])
+        titlu = comanda[2]
+        gen = comanda[3]
+        pret = float(comanda[4])
+        tip = comanda[5]
+        return update(vanzari, creeaza_vanzare(id, titlu, gen, pret, tip))
+    except ValueError as ve:
+        print('Eroare:', ve)
+    return vanzari
+
+
+def new_menu(vanzari):
+    while True:
+        print("Intr-o linie de comanda se vor scrie comenzile "
+              "care se vor aplica listei, separate prin ';', elementele acestora fiind separate prin ','.")
+        print("Atentie ! Comanda se face scrie cu majuscula si dupa fiecare separator se va pune un spatiu."
+              "O comanda care nu se regaseste in lista de mai jos "
+              "nu va duce la modificarea listei. \n ")
+        print("1.Pentru adaugare in lista: Adaugare")
+        print("2.Pentru stergerea unei vanzari: Sterge")
+        print("3.Pentru modificarea unei vanzari: Modificare")
+        print("4.Pentru afisarea tuturor vanzarilor: ShowAll")
+        print("5. Pentru a iesire din meniu: Exit (la final)")
+        lst_cmd = read_list()
+        for comanda in lst_cmd:
+            comanda = comanda.split(', ')
+            if (comanda[0] == 'Adaugare'):
+                vanzari = handle_newadd(vanzari, comanda)
+            elif (comanda[0] == 'Sterge'):
+                vanzari = handle_newdelete(vanzari, comanda)
+            elif (comanda[0] == 'Modificare'):
+                vanzari = handle_newupdate(vanzari, comanda)
+            elif (comanda[0] == 'ShowAll'):
+                handle_show_all(vanzari)
+        if 'Exit' in lst_cmd:
+            break
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from Logic.crud import create
 from Tests.test_change_genre import test_alt_genre
 from Tests.test_crud import test_crud
 from Tests.test_discount import test_discount
+from UserInterface.command_line_console import new_menu
 from UserInterface.console import run_ui
 
+def menus():
+    print('1. Meniul standard')
+    print('2. Meniul nou')
+    print('x. Exit')
 
 def main():
     vanzari = []
@@ -13,7 +18,18 @@ def main():
     vanzari = create(vanzari, 4, 'Shining', 'Horror', 27.49, 'silver')
     vanzari = create(vanzari, 5, 'A game of thrones', 'Fantasy', 60, 'silver')
     vanzari = create(vanzari, 6, 'Narnia', 'Fantasy', 13.99, 'gold')
-    vanzari = run_ui(vanzari)
+
+    while True:
+        menus()
+        optiune = input('Alegeti meniul: ')
+        if optiune == '1':
+            vanzari = run_ui(vanzari)
+        elif optiune == '2':
+            vanzari = new_menu(vanzari)
+        elif optiune == 'x':
+            break
+        else:
+            print("Optiune invalida ")
 
 
 if __name__ == '__main__':
